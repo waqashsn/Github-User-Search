0089bf26b7f2e9df0f46fcc5868e6f998c8f3a56
WH <waqashsn@gmail.com>
2018-04-15 21:34:35 +0500

    Added user bio, location, company, url, email and counts for repos, followers and following.

--- js/main.js
+++ js/main.js
@@ -12,17 +12,37 @@ $(document).ready(function(){
            }
        }).done(function(user){
            $('#profile').html(`
-           <div class="card text-white bg-primary mb-3">
+           <div class="card text-black bg-secondary mb-3">
                 <div class="card-body">
                     
                     <div class="row">
-                        <div class="col-md-3">
+                        <div class="col-md-2">
                             <a href="${user.html_url}"><img src="${user.avatar_url}" class="img-thumbnail border" width="100%"></a>
                             <a href="${user.html_url}" target="_blank"  class="btn btn-secondary btn-block">View Profile</a>
                         </div>
-                        <div class="col-md-9">
+                        <div class="col-md-10">
                             <h2 class="card-title">${user.name}</h2>
-                            <p>${user.bio}</p>
+                            <blockquote class="blockquote">
+                            <p class="mb-0">${user.bio}</p>    
+                            </blockquote>
+                            
+                            <div class="row">
+                                <div class="col-md-3">
+                                    Repositories <span class="badge badge-light">${user.public_repos}</span><br>
+                                    Location: ${user.location}
+                                </div>
+                                <div class="col-md-2">
+                                    Followers <span class="badge badge-light">${user.followers}</span><br>
+                                    Company: ${user.company}
+                                </div>
+                                <div class="col-md-3">
+                                    Following <span class="badge badge-light">${user.following}</span>
+                                </div>
+                                <div class="col-md-4">
+                                    URL: <a href="" target="_blank">${user.html_url}</a>
+                                    Email: ${user.email}
+                                </div>
+                            </div>
                         </div>
 
                     </div>
