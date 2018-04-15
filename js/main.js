836069a1d0dd50f7e0b8ebedd584365c558533fd
WH <waqashsn@gmail.com>
2018-04-15 18:43:28 +0500

    Added profiles, including user  pictures and bio.

--- js/main.js
+++ js/main.js
@@ -11,7 +11,24 @@ $(document).ready(function(){
                client_secret:'ad0e84007a38f87a60ddccd6a02d288f094763b5'
            }
        }).done(function(user){
-           console.log(user);
+           $('#profile').html(`
+           <div class="card text-white bg-primary mb-3">
+                <div class="card-body">
+                    
+                    <div class="row">
+                        <div class="col-md-3">
+                            <a href="${user.html_url}"><img src="${user.avatar_url}" class="img-thumbnail border" width="100%"></a>
+                            <a href="${user.html_url}" target="_blank"  class="btn btn-secondary btn-block">View Profile</a>
+                        </div>
+                        <div class="col-md-9">
+                            <h2 class="card-title">${user.name}</h2>
+                            <p>${user.bio}</p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+         `);
 
        });
    }) 
